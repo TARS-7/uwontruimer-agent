ccd07f62954060556a8b9aadb68a92bfb627377f
TARS-7 <jekuijper@hotmail.com>
2026-05-22 11:50:09 +0200

    feat: Google Tag Manager GTM-TBW9TTF

--- uwontruimer-site/src/app/layout.tsx
+++ uwontruimer-site/src/app/layout.tsx
@@ -38,8 +38,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         {/* Consent Mode v2 default — moet vóór gtag script staan */}
         <script dangerouslySetInnerHTML={{ __html: consentDefault }} />
+        <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TBW9TTF');`}} />
       </head>
       <body className="bg-slate-50 text-slate-900 antialiased">
+        <noscript dangerouslySetInnerHTML={{__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TBW9TTF" height="0" width="0" style="display:none;visibility:hidden"></iframe>`}} />
         {children}
         <CookieBanner />
         <MetaPixel />
